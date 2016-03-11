fda58981b36749dd166946f15feda6bb4064c721
Thomas Löffler <loeffler@spooner-web.de>
2016-03-11 14:54:50 +0100

    [TASK] Adds config.ts for basic configuration

---
+++ htdocs/typo3conf/ext/t3theme_typo3com/Configuration/TypoScript/config.ts
@@ -0,0 +1,27 @@
+config {
+    doctype = html5
+    language = en
+    linkVars = L(0-1)
+    sys_language_uid = 0
+    locale_all = en_GB.utf8
+    absRefPrefix = /
+    index_enable = 1
+    disablePrefixComment = 1
+
+    htmlTag_setParams = lang="en" class="no-js"
+    sendCacheHeaders = 1
+    pageTitleFirst = 1
+    inlineStyle2TempFile = 0
+
+    spamProtectEmailAddresses = ascii
+
+    typolinkEnableLinksAcrossDomains = 1
+
+#    compressJS = 1
+#    compressCSS = 1
+
+    removeDefaultCss = 1
+    removeDefaultJS = 1
+
+    sys_language_mode = strict
+}
