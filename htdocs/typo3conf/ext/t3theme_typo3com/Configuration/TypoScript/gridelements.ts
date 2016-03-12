eac411f3b657f1837954f04b344c81e200ead538
Christiane Helmchen <helmchen@punkt.de>
2016-03-12 14:51:04 +0100

    [TASK] move TS for custom gridelements

--- htdocs/typo3conf/ext/t3theme_typo3com/Configuration/TypoScript/gridelements.ts
+++ htdocs/typo3conf/ext/t3theme_typo3com/Configuration/TypoScript/gridelements.ts
@@ -1,17 +1,14 @@
 tt_content.gridelements_pi1 {
-    20.10.setup {
-        1 < lib.gridelements.defaultGridSetup
-        1 {
-            columns {
-                # colPos from BE grid layout
-                1 < .default
-                1.wrap = <div class="col-md-4 cols_1">|</div>
+	20.10.setup {
+		1 < lib.gridelements.defaultGridSetup
+		1 {
+			columns {
+				# colPos from BE grid layout
+				1 < .default
+				1.wrap = <div class="col-md-4 cols_1">|</div>
+			}
 
-
-
-            }
-
-            wrap = <div class="row hannebmbel">|</div>
-        }
-      }
-    }
+			wrap = <div class="row hannebmbel">|</div>
+		}
+	}
+}
