39bac8d042f5e69b3150771c54c5454255d99259
Fabian Stein <stein@punkt.de>
2016-03-12 12:35:33 +0100

    WIP Gridelement Feature

---
+++ htdocs/typo3conf/ext/t3theme_typo3com/Configuration/TypoScript/gridelements.ts
@@ -0,0 +1,13 @@
+tt_content.gridelements_pi1 {
+    20.10.setup {
+        1 < lib.gridelements.defaultGridSetup
+        1 {
+            columns {
+                # colPos from BE grid layout
+                1 < .default
+                1.wrap = <div class="col-md-4 cols_1">|</div>
+
+            }
+
+            wrap = <div class="row hannebmbel">|</div>
+        }
