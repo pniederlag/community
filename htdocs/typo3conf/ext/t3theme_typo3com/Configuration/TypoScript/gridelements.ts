91f2722016d09338dbf6b8c84b6b45fd6e8ddbb8
Fabian Stein <stein@punkt.de>
2016-03-12 16:00:34 +0100

    WIP Styling Features

--- htdocs/typo3conf/ext/t3theme_typo3com/Configuration/TypoScript/gridelements.ts
+++ htdocs/typo3conf/ext/t3theme_typo3com/Configuration/TypoScript/gridelements.ts
@@ -5,17 +5,17 @@ tt_content.gridelements_pi1 {
 			columns {
 				# colPos from BE grid layout
 				1 < .default
-				1.wrap = <div class="col-md-4 feature-start feature-first-line">|</div>
+				1.wrap = <div class="col-md-4"><div class="feature-start feature-first-line">|</div></div>
 				2 < .default
-				2.wrap = <div class="col-md-4 feature-start feature-first-line">|</div>
+				2.wrap = <div class="col-md-4"><div class="feature-start feature-first-line">|</div></div>
 				3 < .default
-				3.wrap = <div class="col-md-4 feature-start feature-first-line">|</div>
+				3.wrap = <div class="col-md-4"><div class="feature-start feature-first-line">|</div></div>
 				4 < .default
-				4.wrap = <div class="col-md-4 feature-start">|</div>
+				4.wrap = <div class="col-md-4"><div class="feature-start">|</div></div>
 				5< .default
-				5.wrap = <div class="col-md-4 feature-start">|</div>
+				5.wrap = <div class="col-md-4"><div class="feature-start">|</div></div>
 				6 < .default
-				6.wrap = <div class="col-md-4 feature-start">|</div>
+				6.wrap = <div class="col-md-4"><div class="feature-start">|</div></div>
 				7 < .default
 				7.wrap = <div class="col-md-12 feature-head">|</div>
 
