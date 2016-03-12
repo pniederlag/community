26fc39b010c8294472fba6ed8b706b2a7d99f7ca
Fabian Stein <stein@punkt.de>
2016-03-12 15:28:06 +0100

    WIP Styling Features

--- htdocs/typo3conf/ext/t3theme_typo3com/Configuration/TypoScript/gridelements.ts
+++ htdocs/typo3conf/ext/t3theme_typo3com/Configuration/TypoScript/gridelements.ts
@@ -5,7 +5,20 @@ tt_content.gridelements_pi1 {
 			columns {
 				# colPos from BE grid layout
 				1 < .default
-				1.wrap = <div class="col-md-4 cols_1">|</div>
+				1.wrap = <div class="col-md-4 feature-start">|</div>
+				2 < .default
+				2.wrap = <div class="col-md-4 feature-start">|</div>
+				3 < .default
+				3.wrap = <div class="col-md-4 feature-start">|</div>
+				4 < .default
+				4.wrap = <div class="col-md-4 feature-start">|</div>
+				5< .default
+				5.wrap = <div class="col-md-4 feature-start">|</div>
+				6 < .default
+				6.wrap = <div class="col-md-4 feature-start">|</div>
+				7 < .default
+				7.wrap = <div class="col-md-12 feature-head">|</div>
+
 			}
 
 		}
