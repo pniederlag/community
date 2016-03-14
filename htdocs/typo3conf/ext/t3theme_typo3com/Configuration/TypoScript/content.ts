fed9b221085bdafd4ba4a69f59c03e1e6d3488e5
root <root@t3demo.brainlogical.de>
2016-03-14 12:45:49 -0400

    insert new Text-Media-Indent contentelement and rendering

--- htdocs/typo3conf/ext/t3theme_typo3com/Configuration/TypoScript/content.ts
+++ htdocs/typo3conf/ext/t3theme_typo3com/Configuration/TypoScript/content.ts
@@ -33,6 +33,30 @@ tt_content.casestudy_teaser {
 	file = EXT:t3theme_typo3com/Resources/Private/Templates/ContentElements/CaseStudyTeaser.html
 }
 
+tt_content.indented_textmedia = FLUIDTEMPLATE
+tt_content.indented_textmedia {
+	file = EXT:t3theme_typo3com/Resources/Private/Templates/ContentElements/IndentedTextmedia.html
+	partialRootPath = EXT:t3theme_typo3com/Resources/Private/Partials/ContentElements/
+	dataProcessing {
+		10 = TYPO3\CMS\Frontend\DataProcessing\FilesProcessor
+		10 {
+			references.fieldName = assets
+		}
+
+		20 = TYPO3\CMS\Frontend\DataProcessing\GalleryProcessor
+		20 {
+			maxGalleryWidth = {$styles.content.textmedia.maxW}
+			maxGalleryWidthInText = {$styles.content.textmedia.maxWInText}
+			columnSpacing = {$styles.content.textmedia.columnSpacing}
+			borderWidth = {$styles.content.textmedia.borderWidth}
+			borderPadding = {$styles.content.textmedia.borderPadding}
+		}
+	}
+
+	file = EXT:t3theme_typo3com/Resources/Private/Templates/ContentElements/IndentedTextmedia.html
+}
+
+
 
 tt_content {
 
